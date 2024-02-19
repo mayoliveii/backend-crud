b3c7ae30696eaffe105291606fc3a67a494f4ada
mayoliveii <mayoliveirablv@gmail.com>
2024-02-19 18:17:47 -0300

    fix: adjusts server config

--- src/server.ts
+++ src/server.ts
@@ -19,8 +19,9 @@ const registerPlugins = async (app: FastifyInstance) => {
 
 const startServer = async (app: FastifyInstance) => {
   try {
-    await app.listen({ port: 3000 });
+    await app.listen({ port: 3000, host: "0.0.0.0" });
   } catch (err) {
+    console.error(err);
     process.exit(1);
   }
 };
